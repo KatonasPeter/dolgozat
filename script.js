630fb3132e682bf479ca70dfa49dc7221c4fdbff
KatonasPeter <164053261+KatonasPeter@users.noreply.github.com>
2024-05-28 19:06:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeBackground() {
+    var div = document.getElementById('backgroundDiv');
+    div.style.backgroundImage = 'url("hoka-zinal-13085-1643565794.jpg")';
+    setTimeout(function(){
+      div.style.backgroundImage = 'url("allbirds-sneakers-review-tree-runnerjpg.jpg")';
+    }, 2000);
+  }
+  
